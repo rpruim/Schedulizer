65b98520dae531e58ef761333d9e390ffb4f1ae9
Randall Pruim <rpuim@gmail.com>
2020-02-08 22:31:24 -0500

    initial go at 2.0

---
+++ docs/schedule-bottom.js
@@ -0,0 +1,153 @@
+// javascrtipt to run after HTML has been processed.
+
+// declarations and initialization
+let schedule = []
+let morphTime = 1000
+let width = window.innerWidth * 0.9
+let height = window.innerHeight * 0.3
+let termHeight = height * 0.3
+let roomScale, instructorScale, timeScale, colorScale
+
+// setup
+resizeSvg()
+updateScales(schedule)
+
+d3.select('button.section.add').on('click', () => {
+  let days = checkBoxes('input.days-checkbox')
+  let desc = days.map(d => ({
+    prefix: d3.select('input.section.prefix').property('value'),
+    number: d3.select('input.section.number').property('value'),
+    section: d3.select('input.section.letter').property('value'),
+    instructor: d3.select('input.section.instructor').property('value'),
+    startTimeStr: d3.select('input.section.start.time').property('value'),
+    startTime: time_to_date(
+      d3.select('input.section.start.time').property('value')
+    ),
+    endTimeStr: d3.select('input.section.end.time').property('value'),
+    endTime: time_to_date(
+      d3.select('input.section.end.time').property('value')
+    ),
+    room: d3.select('input.section.room').property('value'),
+    term: d3.select('input[name="term"]:checked').property('value'),
+    day: d,
+  }))
+  console.log(desc)
+  addSessions(desc)
+})
+/**
+ *
+ * @param {String} selection d3 slection string
+ *
+ * side:effect: set height/width of selected elements relative to browser size
+ */
+function resizeSvg(selection = 'svg') {
+  width = Math.round(window.innerWidth * 0.9)
+  height = Math.round(window.innerHeight * 0.3)
+  d3.selectAll(selection)
+    .attr('width', width)
+    .attr('height', height)
+}
+
+function updateScales(schedule) {
+  roomScale = d3
+    .scaleBand()
+    .domain(schedule.map(d => d.room))
+    .range([0, width])
+    .padding(0.1)
+
+  dayInRoomScale = d3
+    .scaleBand()
+    .domain(['M', 'T', 'W', 'R', 'F'])
+    .range([0, roomScale.bandwidth()])
+    .padding(0.05)
+
+  instructorScale = d3
+    .scaleBand()
+    .domain(schedule.map(d => d.instructor))
+    .range([0, width])
+    .padding(0.1)
+
+  dayInInstructorScale = d3
+    .scaleBand()
+    .domain(['M', 'T', 'W', 'R', 'F'])
+    .range([0, instructorScale.bandwidth()])
+    .padding(0.05)
+
+  termScale = d3
+    .scaleBand()
+    .domain(schedule.map(d => d.term))
+    .range([height, 0])
+    .padding(0.1)
+
+  timeScale = d3
+    .scaleTime()
+    .domain([time_to_date('07:00'), time_to_date('21:00')])
+    .range([termHeight, 0])
+}
+
+// add array of sessions
+/**
+ *
+ * @param {array of Objects} desc array of session descriptions
+ */
+function addSessions(desc) {
+  schedule.push(...desc)
+  updateScales(schedule)
+  let svg = d3.select('#schedule-by-room')
+  svg
+    .selectAll('rect.session')
+    .data(schedule)
+    .join(
+      enter => {
+        enter
+          .append('rect')
+          .attr('class', 'session')
+          .attr('x', d => roomScale(d.room) + dayInRoomScale(d.day))
+          .attr('y', d => timeScale(d.endTime))
+          .attr('width', d => dayInRoomScale.bandwidth())
+          .attr('height', d => timeScale(d.startTime) - timeScale(d.endTime))
+      },
+      update => {
+        update
+          .transition()
+          .duration(morphTime)
+          .attr('x', d => roomScale(d.room) + dayInRoomScale(d.day))
+          .attr('y', d => timeScale(d.endTime))
+          .attr('width', d => dayInRoomScale.bandwidth())
+          .attr('height', d => timeScale(d.startTime) - timeScale(d.endTime))
+          .style('fill', 'red')
+      },
+      exit => {
+        exit
+          .transition()
+          .duration(morphTime)
+          .remove()
+      }
+    )
+}
+
+/**
+ *
+ * @param {String} selector d3 selector string
+ * @returns {array of String}} array of values from check boxes
+ */
+function checkBoxes(selector) {
+  var choices = []
+  d3.selectAll(selector).each(function(d) {
+    cb = d3.select(this)
+    if (cb.property('checked')) {
+      choices.push(cb.property('value'))
+    }
+  })
+  return choices
+}
+
+/**
+ *
+ * @param {String} s time as hh:mm
+ * @param {String} base_date a date string as yyyy-mm-dd
+ * @returns {Date}
+ */
+var time_to_date = function(s, base_date = '2000-01-01') {
+  return new Date(base_date + 'T' + s)
+}
